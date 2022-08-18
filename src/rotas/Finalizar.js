f8369c00240f2dd3a74d4f46c7c0826c567cffb8
aridsm <aniemorelanie@gmail.com>
2022-08-18 13:15:29 -0300

    fetch com reducer

--- src/rotas/Finalizar.js
+++ src/rotas/Finalizar.js
@@ -1,72 +1,179 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { useSelector } from 'react-redux';
 import InputField from '../components/InputField'
 import classes from './Finalizar.module.css';
 
+const validateCpf = (cpf) => {
+  const regexCpf = /(?:\d{3}[.-]?){3}\d{2}/
+  return regexCpf.test(cpf)
+}
+
+const initialState = {
+  inputFields: [
+    {
+      nome: 'cep',
+      value: '',
+      placeholder: '00000-000',
+      isValid: false,
+      blur: false,
+      error: ''
+    },
+    {
+      nome: 'bairro',
+      value: '',
+      placeholder: 'Jardim',
+      isValid: false
+    },
+    {
+      nome: 'rua',
+      value: '',
+      placeholder: 'Rua da Lagoa',
+      isValid: false
+    },
+    {
+      nome: 'numero',
+      value: '',
+      placeholder: '176',
+      isValid: false
+    }, {
+      nome: 'complemento',
+      value: '',
+      placeholder: 'apt. 101',
+      isValid: true
+    },
+    {
+      nome: 'nome',
+      value: '',
+      placeholder: 'Ana de Souza',
+      isValid: false
+    },
+    {
+      nome: 'cpf',
+      value: '',
+      placeholder: '000.000.000-00',
+      isValid: false
+    }
+  ],
+  formIsValid: false
+}
+
+const formReducer = (state, action) => {
+
+  if (action.type === 'UPDATE_VALUE') {
+    let newInputField = [...state.inputFields].map(input => {
+      if (input.nome === action.inputName) {
+        input.value = action.value;
+      }
+      return input;
+    })
+
+    if (action.inputName === 'cep') {
+      newInputField = newInputField.map(input => {
+        if (input.nome === 'cep') {
+          input.blur = false;
+        }
+        return input;
+      })
+    }
+
+    return { ...initialState, newInputField }
+  }
+
+  if (action.type === 'CHECK_VALIDITY') {
+    const input = [...state.inputFields].find(input => input.nome === action.inputName);
+    const inputValue = input.value;
+    let isInputValid = inputValue.trim();
+    let newInputField;
+
+    if (action.inputName === 'cpf') {
+      isInputValid = validateCpf(inputValue);
+    }
+
+    if (action.inputName === 'cep') {
+      newInputField = [...state.inputFields].map(input => {
+        if (input.nome === 'cep') {
+          input.blur = true;
+        }
+        return input;
+      })
+
+      return { ...initialState, newInputField }
+    }
+
+    newInputField = [...state.inputFields].map(input => {
+      if (input.nome === action.inputName) {
+        input.isValid = !!isInputValid;
+      }
+      return input;
+    })
+
+    return { ...initialState, newInputField }
+  }
+
+  if (action.type === 'SEND_ERROR') {
+
+    const newInputField = [...state.inputFields].map(input => {
+      if (input.nome === 'cep') {
+        input.error = action.error
+      }
+      return input;
+    })
+
+    return { ...initialState, newInputField }
+  }
+
+  return initialState
+}
+
 
 const Finalizar = () => {
 
-  const [cep, setCep] = useState('')
-  const [cepError, setCepError] = useState(null)
-  const [bairro, setBairro] = useState('')
-  const [rua, setRua] = useState('')
-  const [numero, setNumero] = useState('')
-  const [complemento, setComplemento] = useState('')
-  const [nome, setNome] = useState('')
-  const [cpf, setCpf] = useState('')
-  const [cpfError, setCpfError] = useState(null);
+  const [formState, dispatchForm] = useReducer(formReducer, initialState)
 
-  const itensCarrinho = useSelector(state => state.cart.itens)
+  const itensCarrinho = useSelector(state => state.cart.itens);
 
+  const CEP = formState.inputFields.find(input => input.nome === 'cep');
+  console.log(CEP.blur)
+  useEffect(() => {
 
-  const validateCep = async () => {
-    setCepError(null)
-    try {
-      const response = await fetch(`https://ws.apicep.com/cep/${cep}.json`);
+    if (CEP.blur) {
+      const validateCep = async () => {
 
-      if (!response.ok) {
-        setBairro('');
-        setRua('');
-        throw new Error('Cep invalido');
+        let error = null;
+
+        fetch(`https://ws.apicep.com/cep/${CEP.value}.json`)
+          .then(r => r.json())
+          .then(enderecoData => {
+            if (!enderecoData.ok) {
+              error = enderecoData.message;
+              return;
+            }
+
+            if (enderecoData.city !== 'Rio de Janeiro') {
+              error = 'Este cep nao faz parte da cidade Rio de Janeiro'
+              return;
+            }
+
+            dispatchForm({ type: 'UPDATE_ADDRESS_DATA', bairro: enderecoData.district, rua: enderecoData.address })
+          })
+          .catch(err => {
+            error = err
+          })
+        dispatchForm({ type: 'SEND_ERROR', error })
       }
-
-      const enderecoData = await response.json();
-
-      if (!enderecoData.ok) {
-        setCepError(enderecoData.message)
-        return;
-      }
-
-      if (enderecoData.city !== 'Rio de Janeiro') {
-        setCepError('Este cep nao faz parte da cidade Rio de Janeiro');
-        return
-      }
-
-      setBairro(enderecoData.district);
-      setRua(enderecoData.address);
-
-    } catch (err) {
-      console.log(err);
+      validateCep()
     }
+  }, [CEP.blur, CEP.value])
+
+
+  const updateValue = (e, inputName) => {
+    dispatchForm({ type: 'UPDATE_VALUE', value: e.target.value, inputName })
   }
 
-  const validadeCpf = () => {
-    const cpfRegex = /(?:\d{3}[.-]?){3}\d{2}/g
-    if (!cpfRegex.test(cpf)) {
-      setCpfError('CPF invalido');
-      return;
-    }
-    setCpfError(null)
+  const validateInput = (inputName) => {
+    dispatchForm({ type: 'CHECK_VALIDITY', inputName })
   }
 
-  const submitAddress = (e) => {
-    e.preventDefault();
-    if (cep && bairro && rua && numero && cpf && nome && !cepError && !cpfError) {
-      console.log('form VALIDO')
-      return
-    }
-    console.log('form INVALIDO')
-  }
 
   return (
     <div className={classes.finalizar}>
@@ -74,17 +181,15 @@ const Finalizar = () => {
         <h1>Para onde enviar?</h1>
         <section>
           <p className={classes.cidade}>Cidade: Rio de Janeiro</p>
-          <form onSubmit={submitAddress}>
-            <InputField name='cep' placeholder='Ex: 23587-545' value={cep} setValue={setCep} onBlur={validateCep} />
-            <p className={classes.errorMessage}>{cepError}</p>
-            <InputField name='bairro' placeholder='Jardim' value={bairro} setValue={setBairro} />
-            <InputField name='rua' placeholder='Rua da Lagoa' value={rua} setValue={setRua} />
-            <InputField name='numero' placeholder='123' value={numero} setValue={setNumero} />
-            <InputField name='complemento' placeholder='apt. 101' value={complemento} setValue={setComplemento} />
-            <InputField name='Seu nome' placeholder='Ex: Joao da Silva' value={nome} setValue={setNome} />
-            <InputField name='CPF' placeholder='000.000.000-00' value={cpf} setValue={setCpf} onBlur={validadeCpf} />
-            <p className={classes.errorMessage}>{cpfError}</p>
+          <form onSubmit=''>
+            {formState.inputFields.map(input =>
+              <div key={input.nome}>
+                <label htmlFor={input.nome}>{input.nome}</label>
+                <input type='text' placeholder={input.placeholder} id={input.nome} name={input.nome} value={input.value} onChange={(e) => updateValue(e, input.nome)} onBlur={() => validateInput(input.nome)} />
+
+              </div>)}
             <button className='btn-style'>Finalizar compra</button>
+            {CEP.error}
           </form>
         </section>
       </div>
@@ -103,7 +208,7 @@ const Finalizar = () => {
           )}
         </ul>
       </div>
-    </div>
+    </div >
   )
 }
 
